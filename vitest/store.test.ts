1fcb9608cda93848d67171423bfdb4b8aee0d060
andriidatsyk <datsyk.a@nitralabs.com>
2022-07-03 18:16:11 +0300

    install vitest and create first store test

---
+++ vitest/store.test.ts
@@ -0,0 +1,13 @@
+import { assert, expect, it, describe, test, beforeEach } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+import { useStore } from '../stores/counter.js'
+
+describe('Counter Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+  it('increments', () => {
+    const counter = useStore()
+    expect(counter.count).toBe(0)
+  })
+})
