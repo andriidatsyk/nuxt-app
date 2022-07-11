966fccd0411d99b1ed9c424328f6d5dac33102d3
andriidatsyk <datsyk.a@nitralabs.com>
2022-07-11 22:49:27 +0300

    simple test for auth form

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -6,4 +6,22 @@ describe('Auth testing', () => {
     cy.visit('/login')
     cy.url().should('eq', URL + 'login')
   })
+  it('header auth', () => {
+    cy.get('h2').should('have.text', ' Sign in to your account ')
+  })
+  it('email input', () => {
+    cy.get('#email-address')
+  })
+  it('password input', () => {
+    cy.get('#password')
+  })
+  it('remember me', () => {
+    cy.get('#remember-me')
+  })
+  it('forgot your password', () => {
+    cy.contains('Forgot your password?')
+  })
+  it('btn sign in', () => {
+    cy.get('button')
+  })
 })
